4b375af20f06bbda471161e98b9d73f6fc36f431
Nicolas Danielski <nicolasdanielski@gmail.com>
2022-01-31 18:40:27 -0300

    Desafio: Dados do problema

---
+++ DesafioExtra-nicolas.py
@@ -0,0 +1,6 @@
+prateleira = ['arduino','servoMotor', 'potenciometro', 'LEDverm', 'LEDverd', 'resistor1', 'resistor100k']
+precoPrat = [74, 5, 1.9 , 1, 1, 0.1, 0.5]
+
+comprados = ['resistor100k','resistor100k', 'servoMotor', 'arduino', 'arduino', 'potenciometro', 'resistor1', 'LEDverm','LEDverm','LEDverm', 'LEDverd','LEDverd']
+
+precoPago = [0.7,1, 5, 100,74,2.5,0.1, 1,1,2,2,1]
