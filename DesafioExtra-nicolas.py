b2374885101acda311268e252cee3faa687b8e1d
Nicolas Danielski <nicolasdanielski@gmail.com>
2022-01-31 18:45:34 -0300

    Desafio: Versão final

--- DesafioExtra-nicolas.py
+++ DesafioExtra-nicolas.py
@@ -4,3 +4,12 @@ precoPrat = [74, 5, 1.9 , 1, 1, 0.1, 0.5]
 comprados = ['resistor100k','resistor100k', 'servoMotor', 'arduino', 'arduino', 'potenciometro', 'resistor1', 'LEDverm','LEDverm','LEDverm', 'LEDverd','LEDverd']
 
 precoPago = [0.7,1, 5, 100,74,2.5,0.1, 1,1,2,2,1]
+
+m = dict(zip(prateleira, precoPrat))
+c=0
+
+for i in range(12):
+    if m[comprados[i]] != precoPago[i]:
+        c+=1
+
+print("A quantidade de erros foi: " + str(c))
