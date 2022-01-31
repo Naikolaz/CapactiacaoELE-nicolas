8e0c1155f72d218f6c2f325f84bb2d9ea63be2aa
Nicolas Danielski <nicolasdanielski@gmail.com>
2022-01-31 18:04:07 -0300

    Teste soma

--- Desafio2-nicolas.cpp
+++ Desafio2-nicolas.cpp
@@ -5,5 +5,19 @@ using namespace std;
 
 int main()
 {
+    char op;
+    float a, b;
+
+    cout << "Qual operação você deseja?" << endl;
+    cin >> op;
+    
+    cout << "Insira o primeiro número:" << endl;
+    cin >> a;
+
+    cout << "Insira o segundo número:" << endl;
+    cin >> b;
+
+    cout << "O resultado é: " << (a + b);
+    
     return 0;
 }
